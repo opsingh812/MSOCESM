ad83216a51c99df535f14779b313f08cba9dd2c2
opsingh812 <os00437506@techmahindra.com>
2021-05-24 17:40:51 +0530

    First Commit

---
+++ Experiment/src/com/lab/Experiment2.java
@@ -0,0 +1,9 @@
+package com.lab;
+
+public class Experiment2 {
+
+	public static void main(String[] args) {
+		System.out.print("This is Experiment 2");
+	}
+
+}
---
+++ Experiment/src/com/lab/Experiment3.java
@@ -0,0 +1,9 @@
+package com.lab;
+
+public class Experiment3 {
+
+	public static void main(String[] args) {
+		System.out.print("This is Experiment 3 has failed");
+	}
+
+}
